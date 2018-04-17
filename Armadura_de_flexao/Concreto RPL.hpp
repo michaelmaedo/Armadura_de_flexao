15f7ebac5f17a724429238a2131047afa444772b
Michael Maedo <michaelmaedo@gmail.com>
2018-04-17 15:01:46 -0500

    This program was created a long time ago (2011)

---
+++ Armadura_de_flexao/Concreto RPL.hpp
@@ -0,0 +1,32 @@
+[Main]
+library=0
+IncludeUserRPL=1
+AutoLoad=0
+Compress=0
+Calc48=0
+KeepAll=0
+UseDefaultEntryList=1
+WarnRamChanges=1
+LoadOnly=0
+EMUVisible=1
+ProjectTab=0
+InformHeight=50
+EntryPointList=
+Title=New_Project
+Config=
+ExtPrg=
+Message=
+Romid=200
+Slot=0
+Emu48Ram=C:\Documents and Settings\Michael\Meus documentos\Debug4x\Emu\default 48GX.e48
+Emu48Port2=C:\Arquivos de programas\Hewlett-Packard\Debug4x\Emu\default 48GX.bin
+Emu49Ram=C:\Documents and Settings\Michael\Meus documentos\Debug4x\Emu\default 50G.e49
+FilePath=Concreto RPL.hp
+DirPath=
+RPL_INCLUDE=
+SasmParams=
+LRFiles=
+[Files]
+file1=Concreto RPL.s
+[Edit]
+edit2=>Concreto RPL.s
